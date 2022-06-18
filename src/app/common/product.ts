f887113fb7c83c922a7214b81b5989a13a1ff39f
Daniel Tamang <daniel.tamang@f1soft.com>
2022-06-18 09:28:12 +0545

    initial commit

---
+++ src/app/common/product.ts
@@ -0,0 +1,23 @@
+export class Product {
+  sku: string;
+  name: string;
+  description: string;
+  unitPrice: number;
+  imageUrl: string;
+  active: boolean;
+  unitsInStock: string;
+  dateCreated: string;
+  lastUpdated: string;
+
+  constructor(sku: string, name: string, description: string, unitPrice: number, imageUrl: string, active: boolean, unitsInStock: string, dateCreated: string, lastUpdated: string) {
+    this.sku = sku;
+    this.name = name;
+    this.description = description;
+    this.unitPrice = unitPrice;
+    this.imageUrl = imageUrl;
+    this.active = active;
+    this.unitsInStock = unitsInStock;
+    this.dateCreated = dateCreated;
+    this.lastUpdated = lastUpdated;
+  }
+}
